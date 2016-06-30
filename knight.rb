0d170e8a7bfc3b07cd2d4af32e31d0cdce9ae050
urzp <urzp002@mail.ru>
2016-06-30 20:01:38 +0300

    Rewrite new method

--- knight.rb
+++ knight.rb
@@ -1,12 +1,15 @@
 
 class knight
   attr_accessor :position
-  
+
   def initilaze(x = 1, y = 1)
     @position {x: x, y: y}
   end
-  
-  def knight_moves(start, finish)
-    return route
-  end
+end
+
+def knight_moves(start, finish)
+  return route
+end
+
+def knight_allmoves(start)
 end
