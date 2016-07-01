71eebbeca5b8e30b9161cd21487595ef5c00902e
urzp <urzp002@mail.ru>
2016-07-01 09:29:07 +0300

    Create Node class and test

--- knight.rb
+++ knight.rb
@@ -1,11 +1,14 @@
 
-class knight
-  attr_accessor :position
+class Node
+  attr_accessor :position, :parent
 
-  def initilaze(x = 1, y = 1)
-    @position {x: x, y: y}
+  def initialize(parent, position)
+    @parent = parent
+    @position = position
   end
 end
+
+
 
 def knight_moves(start, finish)
   return route
@@ -13,3 +16,5 @@ end
 
 def knight_allmoves(start)
 end
+
+n = Node.new("root", [3,3])
